3134ffa2278fbcd1d6aaa183d8def4746e121ec4
La Minh Duc <laminhduc200@gmail.com>
2024-01-04 00:28:39 +0700

    update display result

--- ques2.py
+++ ques2.py
@@ -4,20 +4,23 @@ import matplotlib.pyplot as mpl
 # input graph
 def input_graph(G):
     # add node
-    G.add_node("0", demand=0)
-    G.add_node("1", demand=-13)
+    G.add_node("0", demand=-10)
+    G.add_node("1", demand=-10)
     G.add_node("2", demand=0)
     G.add_node("3", demand=0)
-    G.add_node("4", demand=13)
+    G.add_node("4", demand=20)
+    G.add_node("5", demand=0)
 
     #add egde
     G.add_edge("0", "1", weight=2, capacity=10)
     G.add_edge("0", "2", weight=6, capacity=5)
     G.add_edge("1", "2", weight=1, capacity=15)
     G.add_edge("1", "3", weight=3, capacity=9)
-    G.add_edge("2", "3", weight=1, capacity=10)
+    G.add_edge("2", "3", weight=4, capacity=10)
     G.add_edge("2", "4", weight=3, capacity=10)
     G.add_edge("3", "4", weight=5, capacity=5)
+    G.add_edge("3", "5", weight=5, capacity=10)
+    G.add_edge("5", "4", weight=2, capacity=15)
 
 # detect negative cycle
 def have_neg_cycle(G, source, sink):
@@ -50,19 +53,19 @@ def printPath(flowDict, G):
 
                 total_cost += flow_on_path * cost_per_flow
 
-                print("__________________________________________________________________")
+                print("________________________________________________________")
                 print("Path:", path_str)
                 print("Flow sent on this path:", flow_on_path)
                 print("Cost per flow on this path:", cost_per_flow)
 
-    print("__________________________________________________________________")
+    print("________________________________________________________")
     print("Paths found:")
     for source, neighbors in flowDict.items():
         for target, flow in neighbors.items():
             if flow > 0:
                 path = nx.shortest_path(G, source=source, target=target, weight="weight")
                 path_str = " -> ".join(path)
-                print(path_str)
+                print(path_str + '\t' + str(flow))
 
     print("Total Cost:", total_cost if total_cost != float('inf') else None)
 
@@ -71,14 +74,13 @@ sink = []
 G = nx.DiGraph()
 input_graph(G)
 have_neg_cycle(G, source, sink)
-
 position = nx.circular_layout(G)
-
 nx.draw(G, with_labels=True, font_color='red', node_size=800, pos=position)
-nx.draw_networkx_edge_labels(G, pos=position, edge_labels=nx.get_edge_attributes(G, 'weight'))
+nx.draw_networkx_edge_labels(G, pos=position, edge_labels=nx.get_edge_attributes(G, 'capacity'))
 flowCost, flowDict = nx.capacity_scaling(G)
 
 # Print result
 printPath(flowDict, G)
 
+mpl.savefig("Graph.png")
 mpl.show()
