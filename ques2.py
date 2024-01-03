2b0bcf5f218c3205b709ef8b74bf2ffee1bbbcbb
La Minh Duc <laminhduc200@gmail.com>
2024-01-03 21:59:06 +0700

    add except negative cycle, weight in edge

--- ques2.py
+++ ques2.py
@@ -19,12 +19,35 @@ def input_graph(G):
     G.add_edge("2", "4", weight=3, capacity=10)
     G.add_edge("3", "4", weight=5, capacity=5)
 
+# detect negative cycle
+def have_neg_cycle(G, source, sink):
+    for node in G.nodes(data=True):
+        if node[1]['demand'] > 0:
+            sink += [node[0]]
+        elif node[1]['demand'] < 0:
+            source += [node[0]]
 
+    for node in source:
+        try: 
+            nx.find_negative_cycle(G, source='1') 
+        except nx.NetworkXError:
+            pass
+        else:
+            print("Graph have negative cycle")
+            exit()
+
+source = []
+sink = []
 G = nx.DiGraph()
 input_graph(G)
-nx.draw(G, with_labels=True, font_color='red')
-
+have_neg_cycle(G, source, sink)
+position = nx.circular_layout(G)
+nx.draw(G, with_labels=True, font_color='red', node_size=800, pos=position)
+nx.draw_networkx_edge_labels(G, pos=position, edge_labels=nx.get_edge_attributes(G, 'weight'))
 flowCost, flowDict = nx.capacity_scaling(G)
+
+# Print result
 print(flowDict)
+print(flowCost)
 
 mpl.show()
